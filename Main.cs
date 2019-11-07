81480aa0914a286eba96501dca408cdb0f6772a7
Maxsior <maxsior@mail.ru>
2019-11-07 21:57:22 +0500

    initial commit

---
+++ Main.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class Program
+{
+	public static void Main(string[] args)
+	{
+        var graph = Reader.Read("data/1.mis");
+        
+	}
+}
---
+++ Reader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+public static class Reader
+{
+	public static int[,] Read(string fname)
+	{
+
+        int[,] graph = null;
+        StreamReader sr = new StreamReader(fname);
+        string[] line;
+        while (!sr.EndOfStream)
+        {
+            line = sr.ReadLine().Split(' ');
+
+            if (line[0] == "p")
+            {
+                int n = int.Parse(line[2]);
+                graph = new int[n, n];
+            }
+            else if (line[0] == "e")
+            {
+                if (graph == null)
+                {
+                    throw new Exception("Чо за фигня? Где строка с описание количества вершин графа?");
+                }
+
+                int i = int.Parse(line[1]) - 1;
+                int j = int.Parse(line[2]) - 1;
+
+                graph[i, j] = 1;
+                graph[j, i] = 1;
+            }
+        }
+        sr.Close();
+
+        return graph;
+	}
+}
