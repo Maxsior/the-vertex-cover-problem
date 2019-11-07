bce0f3c8284cc64c2276db7d27286bb198043aa4
Damir <damir.khanof@yandex.ru>
2019-11-07 23:25:57 +0500

    Lazy algorithm implimentation

--- Main.cs
+++ Main.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using VertexCoverProblem;
 
 public static class Program
 {
 	public static void Main(string[] args)
 	{
-        var graph = Reader.Read("data/1.mis");
-        
+        var graph = Reader.Read("data/LazySampleEasy.mis");
+
+        LazyAlg lazy = new LazyAlg(graph);
+
+        List<int> res = lazy.Solve();
+
+        foreach(int x in res)
+        {
+            Console.WriteLine(x);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine(res.Count);
 	}
 }
