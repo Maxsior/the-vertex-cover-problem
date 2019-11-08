07ae520a48e359f39af7b1eebb5051d76e7e1661
Maxsior <maxsior@mail.ru>
2019-11-08 13:18:18 +0500

    release explicit algorytm

--- Main.cs
+++ Main.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 
-public static class Program
-{
-	public static void Main(string[] args)
-	{
-        var graph = Reader.Read("data/1.mis");
-        
-	}
+namespace VertexCoverProblem {
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var graph = Reader.Read("data/1.mis");
+            var explicitSolver = new ExplicitAlgorytm(graph);
+            var vertexCover = explicitSolver.Solve();
+            Console.Write("Минимальное вершинное покрытие: ");
+            foreach (var v in vertexCover)
+            {
+                Console.Write(v + " ");
+            }
+            Console.WriteLine();
+        }
+    }
 }
